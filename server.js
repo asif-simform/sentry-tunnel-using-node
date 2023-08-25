23b55a131e476ab06c12fc543ef1270f0b68150e
asif-simform <Asif.V@simformsolutions.com>
2023-08-25 10:57:37 +0530

    fix: Extend payload limits

--- server.js
+++ server.js
@@ -5,21 +5,20 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3005;
 
-app.use(bodyParser.text({ type: ['text/*', '*/json']}))
+app.use(bodyParser.text({ type: ['text/*', '*/json'], limit: '50mb' }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// TODO: change this according your needs
+const sentryKey = '1cde1991677ab0bee929938ca9136004';
+const sentryHost = 'o4505635479945216.ingest.sentry.io';
+const projectId = '4505635481190400';
+const url = `https://${sentryHost}/api/${projectId}/envelope/?sentry_key=${sentryKey}`;
+
 app.post('/tunnel', (req, res) => {
     const envelope = req.body;
-
-    // https://1cde1991677ab0bee929938ca9136004@o4505635479945216.ingest.sentry.io/4505635481190400
-    const auth = '1cde1991677ab0bee929938ca9136004';
-    const sentryHost = 'o4505635479945216.ingest.sentry.io';
-    const projectId = '4505635481190400';
-    const url = `https://${auth}@${sentryHost}/api/${projectId}/envelope/`;
-
     const options = {
         'method': 'POST',
         'url': url,
@@ -28,9 +27,10 @@ app.post('/tunnel', (req, res) => {
         },
         body: envelope
     };
+    
     request(options, function (error, response) {
         if (error) {
-            const message =  error?.response || error?.message;
+            const message = error?.response || error?.message;
             res.status(422).json({ message: "Something went wrong", error: message });
         } else {
             res.status(201).json({ message: "Success", data: response?.body })
