37595bc31af37d689f3b24238a9972f4284f9aae
asif-simform <Asif.V@simformsolutions.com>
2023-08-25 11:09:27 +0530

    add try and catch

--- server.js
+++ server.js
@@ -15,27 +15,31 @@ app.get('/', (req, res) => {
 const sentryKey = '1cde1991677ab0bee929938ca9136004';
 const sentryHost = 'o4505635479945216.ingest.sentry.io';
 const projectId = '4505635481190400';
-const url = `https://${sentryHost}/api/${projectId}/envelope/?sentry_key=${sentryKey}`;
 
 app.post('/tunnel', (req, res) => {
-    const envelope = req.body;
-    const options = {
-        'method': 'POST',
-        'url': url,
-        'headers': {
-            'Content-Type': 'application/x-sentry-envelope'
-        },
-        body: envelope
-    };
-    
-    request(options, function (error, response) {
-        if (error) {
-            const message = error?.response || error?.message;
-            res.status(422).json({ message: "Something went wrong", error: message });
-        } else {
-            res.status(201).json({ message: "Success", data: response?.body })
-        }
-    });
+    try {
+        const envelope = req.body;
+        const url = `https://${sentryHost}/api/${projectId}/envelope/?sentry_key=${sentryKey}`;
+        const options = {
+            'method': 'POST',
+            'url': url,
+            'headers': {
+                'Content-Type': 'application/x-sentry-envelope'
+            },
+            body: envelope
+        };
+
+        request(options, function (error, response) {
+            if (error) {
+                const message = error?.response || error?.message;
+                res.status(422).json({ message: "Something went wrong", error: message });
+            } else {
+                res.status(201).json({ message: "Success", data: response?.body })
+            }
+        });
+    } catch (e) {
+        res.status(400).json(JSON.stringify({ status: 'invalid request' }));
+    }
 });
 
 app.listen(port, () => {
