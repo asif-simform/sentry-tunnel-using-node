21996aac008611d00883a766d1478d89fcb0db07
asif-simform <Asif.V@simformsolutions.com>
2023-08-25 11:32:19 +0530

    feat: Add axios for API's call

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+import axios from 'axios';
 import express from 'express';
-import request from 'request';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -16,29 +16,22 @@ const sentryKey = '1cde1991677ab0bee929938ca9136004';
 const sentryHost = 'o4505635479945216.ingest.sentry.io';
 const projectId = '4505635481190400';
 
-app.post('/tunnel', (req, res) => {
+app.post('/tunnel', async (req, res) => {
     try {
         const envelope = req.body;
         const url = `https://${sentryHost}/api/${projectId}/envelope/?sentry_key=${sentryKey}`;
         const options = {
-            'method': 'POST',
-            'url': url,
             'headers': {
                 'Content-Type': 'application/x-sentry-envelope'
-            },
-            body: envelope
+            }
         };
 
-        request(options, function (error, response) {
-            if (error) {
-                const message = error?.response || error?.message;
-                res.status(422).json({ message: "Something went wrong", error: message });
-            } else {
-                res.status(201).json({ message: "Success", data: response?.body })
-            }
-        });
+        const response = await axios.post(url, envelope, options);
+
+        res.status(201).json({ message: "Success", data: response?.data })
     } catch (e) {
-        res.status(400).json(JSON.stringify({ status: 'invalid request' }));
+        const error = e?.response || e?.message;
+        res.status(400).json({ message: 'invalid request', error: error });
     }
 });
 
